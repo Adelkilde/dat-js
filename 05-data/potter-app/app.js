098d6e19824be3aa32ae441428ded8a9ac603175
Rasmus Cederdorff <rasmus@cederdorff.com>
2023-03-16 11:42:03 +0100

    renaming

--- 05-data/potter-app/app.js
+++ 05-data/potter-app/app.js
@@ -1,19 +1,17 @@
 "use strict";
-// ============ GLOBAL VARIABELS ============ //
-let characters = [];
 
 // ============ LOAD & INIT APP ============ //
 
 window.addEventListener("load", initApp);
 
 async function initApp() {
-    characters = await readCharacters();
+    const characters = await loadCharacters();
     displayCharacters(characters);
 }
 
 // ============ READ ============ //
 // Read (GET) all users from Firebase (Database) using REST API
-async function readCharacters() {
+async function loadCharacters() {
     const response = await fetch("https://raw.githubusercontent.com/cederdorff/dat-js/main/data/potter.json");
     const data = await response.json();
     console.log(data);
@@ -24,23 +22,27 @@ async function readCharacters() {
 function displayCharacters(characterList) {
     //loop through all users and create an article with content for each
     for (const character of characterList) {
-        document.querySelector("#characters").insertAdjacentHTML(
-            "beforeend",
-            /*html*/ `
+        displayCharacter(character);
+    }
+}
+
+function displayCharacter(character) {
+    document.querySelector("#characters").insertAdjacentHTML(
+        "beforeend",
+        /*html*/ `
             <article>
                 <img src="${character.image}">
                 <h2>${character.name}</h2>
                 <p>${character.house}</p>
             </article>
         `
-        );
-        document
-            .querySelector("#characters article:last-child")
-            .addEventListener("click", () => showCharacter(character));
-    }
+    );
+    document
+        .querySelector("#characters article:last-child")
+        .addEventListener("click", () => showCharacterModal(character));
 }
 
-function showCharacter(character) {
+function showCharacterModal(character) {
     console.log(character);
     document.querySelector("#dialog-title").textContent = character.name;
     document.querySelector("#dialog-house").textContent = character.house;
