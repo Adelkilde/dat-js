da762c34d53d2bd532c24387bcb825a1e8645f3e
peterlinddk <petl@kea.dk>
2023-03-19 13:10:56 +0100

    load data from json and display (also fixed birthday calculation)

--- 05-data/peters-potter-app/script.js
+++ 05-data/peters-potter-app/script.js
@@ -1,101 +1,112 @@
 "use strict";
 
-// data:
-const lucius = {
-    name: "Lucius Malfoy",
-    species: "human",
-    gender: "male",
-    house: "Slytherin",
-    dateOfBirth: "",
-    yearOfBirth: 1954,
-    ancestry: "pure-blood",
-    eyeColour: "grey",
-    hairColour: "blonde",
-    wand: "elm,dragon heartstring,18",
-    patronus: "",
-    hogwartsStudent: false,
-    hogwartsStaff: false,
-    actor: "Jason Isaacs",
-    alive: true,
-    image: "http://hp-api.herokuapp.com/images/lucius.jpg"
+window.addEventListener("load", start);
+
+async function start() {
+  const characters = await loadJSON();
+  const harry = characters.at(0);
+  displayCharacter(harry);
+  const ron = characters.at(2);
+  displayCharacter(ron);
+  const draco = characters.at(3);
+  displayCharacter(draco);
+
+  displayDetails(draco);
+  document.querySelector("dialog").showModal();
+}
+
+
+async function loadJSON() {
+  const response = await fetch("potter.json");
+  const data = await response.json();
+  console.log("Received data:", data);
+  return data;
+}
+
+
+function displayDetails(character) {
+  // name, image, actor
+  document.querySelector("#details-character-name").textContent = character.name;
+  document.querySelector("#details-image img").src = character.image;
+  document.querySelector("#details-image img").alt = `portrait of ${character.name}`;
+  document.querySelector("#details-image figcaption").textContent = `portrayed by actor ${character.actor}`;
+
+  // house
+  document.querySelector("#details-house-name").textContent = character.house;
+  document.querySelector("#details-house-crest").src = `images/housecrest-${character.house.toLowerCase()}.png`;
+  document.querySelector("#details-house-crest").alt = `${character.house} house crest`;
+
+  // hogwarts-status
+  if (!character.hogwartsStudent && !character.hogwartsStaff) {
+    document.querySelector("#details-hogwarts-status .not").classList.remove("hide");
+    document.querySelector("#details-hogwarts-status .teacher").classList.add("hide");
+    document.querySelector("#details-hogwarts-status .student").classList.remove("hide");
+  } else if (character.hogwartsStudent) {
+    document.querySelector("#details-hogwarts-status .not").classList.add("hide");
+    document.querySelector("#details-hogwarts-status .teacher").classList.add("hide");
+    document.querySelector("#details-hogwarts-status .student").classList.remove("hide");
+  } else if (character.hogwartsStaff) {
+    document.querySelector("#details-hogwarts-status .not").classList.add("hide");
+    document.querySelector("#details-hogwarts-status .teacher").classList.remove("hide");
+    document.querySelector("#details-hogwarts-status .student").classList.add("hide");
   }
 
-  function displayDetails( character ) {
-    // name, image, actor
-    document.querySelector("#details-character-name").textContent = character.name;
-    document.querySelector("#details-image img").src = character.image;
-    document.querySelector("#details-image img").alt = `portrait of ${character.name}`;
-    document.querySelector("#details-image figcaption").textContent = `portrayed by actor ${character.actor}`;
+  // species
+  document.querySelector("#details-species").textContent = `${character.species} | ${character.gender} | ${character.ancestry}`;
 
-    // house
-    document.querySelector("#details-house-name").textContent = character.house;
-    document.querySelector("#details-house-crest").src = `images/housecrest-${character.house.toLowerCase()}.png`;
-    document.querySelector("#details-house-crest").alt = `${character.house} house crest`;
+  // wand
+  const wand = character.wand;
+  const wandType = wand.substring(0, wand.indexOf(","));
+  const wandCore = wand.substring(wand.indexOf(",") + 1, wand.lastIndexOf(","));
+  const wandLength = wand.substring(wand.lastIndexOf(",") + 1);
 
-    // hogwarts-status
-    if(!character.hogwartsStudent && !character.hogwartsStaff)  {
-        document.querySelector("#details-hogwarts-status .not").classList.remove("hide");
-        document.querySelector("#details-hogwarts-status .teacher").classList.add("hide");
-        document.querySelector("#details-hogwarts-status .student").classList.remove("hide");
-    } else if(character.hogwartsStudent) {
-        document.querySelector("#details-hogwarts-status .not").classList.add("hide");
-        document.querySelector("#details-hogwarts-status .teacher").classList.add("hide");
-        document.querySelector("#details-hogwarts-status .student").classList.remove("hide");
-    } else if(character.hogwartsStaff) {
-        document.querySelector("#details-hogwarts-status .not").classList.add("hide");
-        document.querySelector("#details-hogwarts-status .teacher").classList.remove("hide");
-        document.querySelector("#details-hogwarts-status .student").classList.add("hide");
-    }
+  document.querySelector("#details-wand-type").textContent = wandType;
+  document.querySelector("#details-wand-core").textContent = wandCore;
+  document.querySelector("#details-wand-length").textContent = wandLength + '"';
 
-    // species
-    document.querySelector("#details-species").textContent = `${character.species} | ${character.gender} | ${character.ancestry}`;
-
-    // wand
-    const wand = character.wand;
-    const wandType = wand.substring(0,wand.indexOf(","));
-    const wandCore = wand.substring(wand.indexOf(",")+1, wand.lastIndexOf(","));
-    const wandLength = wand.substring(wand.lastIndexOf(",")+1);
-
-    document.querySelector("#details-wand-type").textContent = wandType;
-    document.querySelector("#details-wand-core").textContent = wandCore;
-    document.querySelector("#details-wand-length").textContent = wandLength + '"';
-
-    // birthday 
-    const now = new Date();
-    const bday = new Date();
-    if(character.dateOfBirth) {
-        // set bday to date
-        bday.parse(character.dateOfBirth);
-        document.querySelector("#details-birthday .bday").textContent = character.dateOfBirth;
-        document.querySelector("#details-birthday .approx").classList.remove("hide");
-    } else {
-        // set bday to year
-        document.querySelector("#details-birthday .bday").textContent = character.yearOfBirth;
-        document.querySelector("#details-birthday .approx").classList.add("hide");
-        bday.setFullYear(character.yearOfBirth);
-    }
-
-    // calculate age
-    const age = now.getFullYear() - bday.getFullYear();
-    // TODO: Handle bday after or before today
-    document.querySelector("#details-birthday .age").textContent = age;
-
+  // birthday
+  const now = new Date();
+  const bday = new Date();
+  if (character.dateOfBirth) {
+    // set bday to date
+    const day = Number(character.dateOfBirth.substring(0,2));
+    const month = Number(character.dateOfBirth.substring(3,5));
+    const year = Number(character.dateOfBirth.substring(6));
+    bday.setDate(day);
+    bday.setMonth(month-1);
+    bday.setFullYear(year);
+    document.querySelector("#details-birthday .bday").textContent = character.dateOfBirth;
+    document.querySelector("#details-birthday .approx").classList.add("hide");
+  } else {
+    // set bday to year
+    document.querySelector("#details-birthday .bday").textContent = character.yearOfBirth;
+    document.querySelector("#details-birthday .approx").classList.remove("hide");
+    bday.setFullYear(character.yearOfBirth);
   }
 
-  function displayCharacter(character) {
-    const grid = document.querySelector("#character-grid");
-    grid.insertAdjacentHTML("beforeend", getCharacterHTML(character));
+  // calculate age
+  let age = now.getFullYear() - bday.getFullYear();
+  if(bday.getMonth() > now.getMonth() ||
+    (bday.getMonth() == now.getMonth() && bday.getDate() > now.getDate())) {
+    age--;
   }
+  document.querySelector("#details-birthday .age").textContent = age;
+}
 
-  function getCharacterHTML(character) {
-    const html = `<article class="character" data-theme="${character.house.toLowerCase()}">
+function displayCharacter(character) {
+  const grid = document.querySelector("#character-grid");
+  grid.insertAdjacentHTML("beforeend", getCharacterHTML(character));
+}
+
+function getCharacterHTML(character) {
+  const html = `<article class="character" data-theme="${character.house.toLowerCase()}">
         <img src="${character.image}" alt="portrait of ${character.name}">
         <h2>${character.name}</h2>
         <p>${character.house}</p>
     </article>`;
 
-    return html;
-  }
+  return html;
+}
 
-  displayDetails(lucius);
-  displayCharacter(lucius);
+//  displayDetails(lucius);
+//  displayCharacter(lucius);
