b4337fa1efeb5bdc2f5a748efb1c81b551a55783
Rasmus Cederdorff <rasmus@cederdorff.com>
2023-04-17 08:04:26 +0200

    firebase-rest-api added

---
+++ 07-form-rest-and-crud/firebase-rest-api/app.js
@@ -0,0 +1,29 @@
+"use strict";
+
+const endpoint = "https://post-rest-api-default-rtdb.firebaseio.com";
+
+window.addEventListener("load", initApp);
+
+function initApp() {
+    console.log("app.js is running 🎉");
+    getPosts();
+}
+
+async function getPosts() {
+    const res = await fetch(`${endpoint}/posts.json`);
+    const data = await res.json();
+    const posts = preparePostData(data);
+    return posts;
+}
+
+function preparePostData(dataObject) {
+    const postArray = [];
+    for (const key in dataObject) {
+        const post = dataObject[key];
+        post.id = key;
+        postArray.push(post);
+    }
+
+    console.log(postArray);
+    return postArray;
+}
